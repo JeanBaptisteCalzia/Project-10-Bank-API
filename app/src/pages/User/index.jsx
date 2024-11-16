52a4693a008dc5b56c8399a92df329078d4b4791
JBC <hello@jeanbaptistecalzia.fr>
2024-11-16 12:15:23 +0100

    Create Account component

--- app/src/pages/User/index.jsx
+++ app/src/pages/User/index.jsx
@@ -1,4 +1,5 @@
 import Nav from "../../components/Nav/";
+import Account from "../../components/Account/";
 import Footer from "../../components/Footer/";
 import "./user.scss";
 
@@ -6,7 +7,18 @@ function User() {
   return (
     <>
       <Nav />
-      <main className="main bg-dark"></main>
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>
+            Welcome back
+            <br />
+            Tony Jarvis!
+          </h1>
+          <button className="edit-button">Edit Name</button>
+        </div>
+        <h2 className="sr-only">Accounts</h2>
+        <Account />
+      </main>
       <Footer />
     </>
   );
