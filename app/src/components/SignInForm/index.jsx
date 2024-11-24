fbe74e621bd87a306dbf113cb2f4c117b477abdf
JBC <hello@jeanbaptistecalzia.fr>
2024-11-24 16:30:17 +0100

    Add routeChange navigation on Sign In btn click

--- app/src/components/SignInForm/index.jsx
+++ app/src/components/SignInForm/index.jsx
@@ -1,8 +1,15 @@
 import "./signInForm.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 function SignInForm() {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = "/user";
+    navigate(path);
+  };
+
   return (
     <section className="sign-in-content">
       <FontAwesomeIcon classNmae="sign-in-icon" icon={faUserCircle} />
@@ -20,7 +27,9 @@ function SignInForm() {
           <input type="checkbox" id="remember-me" />
           <label htmlFor="remember-me">Remember me</label>
         </div>
-        <button className="sign-in-button">Sign In</button>
+        <button onClick={routeChange} className="sign-in-button">
+          Sign In
+        </button>
       </form>
     </section>
   );
