9720cd2943ed150bfef595d073b0e03324af4228
JBC <hello@jeanbaptistecalzia.fr>
2024-11-25 15:15:09 +0100

    Add form validation for signInForm component

--- app/src/components/SignInForm/index.jsx
+++ app/src/components/SignInForm/index.jsx
@@ -1,33 +1,71 @@
-import "./signInForm.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import "./signInForm.scss";
 
 function SignInForm() {
   let navigate = useNavigate();
-  const routeChange = () => {
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userNameError, setUserNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUserNameError("");
+    setPasswordError("");
+
+    if (userName.length < 3) {
+      setUserNameError("Username must be 3 characters or longer");
+      return;
+    }
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
+    }
+
+    if (password.length < 8) {
+      setPasswordError("The password must be 8 characters or longer");
+      return;
+    }
+
     let path = "/user";
     navigate(path);
-  };
+  }
 
   return (
     <section className="sign-in-content">
-      <FontAwesomeIcon classNmae="sign-in-icon" icon={faUserCircle} />
+      <FontAwesomeIcon className="sign-in-icon" icon={faUserCircle} />
       <h1>Sign In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-wrapper">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <span>{userNameError}</span>
         </div>
         <div className="input-wrapper">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <span>{passwordError}</span>
         </div>
         <div className="input-remember">
           <input type="checkbox" id="remember-me" />
           <label htmlFor="remember-me">Remember me</label>
         </div>
-        <button onClick={routeChange} className="sign-in-button">
+        <button type="submit" className="sign-in-button">
           Sign In
         </button>
       </form>
