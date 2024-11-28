e1a58e8cf6330dfd1b6c67d120aba1bd63cdb56c
JBC <hello@jeanbaptistecalzia.fr>
2024-11-28 21:27:58 +0100

    Refactoring user form component

--- app/src/pages/User/index.jsx
+++ app/src/pages/User/index.jsx
@@ -4,35 +4,85 @@ import Account from "../../components/Account/";
 import Footer from "../../components/Footer/";
 import "./user.scss";
 
+function Form({
+  onChange,
+  onSubmit,
+  onCancel,
+  errorMessage,
+  valueFirstName,
+  valueLastName,
+}) {
+  return (
+    <form onSubmit={onSubmit}>
+      <label className="sr-only" htmlFor="firstName">
+        First name:
+      </label>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        placeholder="Tony"
+        value={valueFirstName}
+        onChange={onChange}
+      />
+      <label className="sr-only" htmlFor="lastName">
+        Last name:
+      </label>
+      <input
+        type="text"
+        id="lastName"
+        name="lastName"
+        placeholder="Jarvis"
+        value={valueLastName}
+        onChange={onChange}
+      />
+      <br />
+      {errorMessage && (
+        <p>Firstname and Name must be greater than 2 characters</p>
+      )}
+      <br />
+      <button type="submit" className="button is--save">
+        Save
+      </button>
+      <button className="button" onClick={onCancel}>
+        Cancel
+      </button>
+    </form>
+  );
+}
+
 function User() {
   const [isEdit, setIsEdit] = useState(false);
-  const [firstName, setFirstName] = useState("Tony");
-  const [lastName, setLastName] = useState("Jarvis");
+  const [formData, setFormData] = useState({
+    firstName: "Tony",
+    lastName: "Jarvis",
+  });
   const [isInputError, setIsInputError] = useState(false);
 
-  function handleChangeFirstName(e) {
-    setFirstName(e.target.value);
-  }
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-  function handleChangeLastName(e) {
-    setLastName(e.target.value);
-  }
+    setFormData((data) => ({
+      ...data,
+      [name]: value,
+    }));
 
-  function cancel() {
-    setIsEdit(!isEdit);
-    setFirstName(firstName);
-    setLastName(lastName);
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (firstName.length < 3 || lastName.length < 3) {
+    if (formData.firstName.length < 3 || formData.lastName.length < 3) {
       setIsInputError(true);
     } else {
-      setFirstName(firstName);
-      setLastName(lastName);
+      setFormData(formData);
       setIsEdit(!isEdit);
     }
+  }
+
+  function handleCancel() {
+    setIsEdit(!isEdit);
   }
 
   return (
@@ -43,46 +93,17 @@ function User() {
           <h1>
             Welcome back
             <br />
-            {!isEdit ? `${firstName} ${lastName} !` : null}
+            {!isEdit ? `${formData.firstName} ${formData.lastName} !` : null}
           </h1>
           {isEdit ? (
-            <form onSubmit={handleSubmit}>
-              <label className="sr-only" htmlFor="firstName">
-                First name:
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                placeholder="Tony"
-                // minLength={3}
-                value={firstName}
-                onChange={handleChangeFirstName}
-              />
-              <label className="sr-only" htmlFor="lastName">
-                Last name:
-              </label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                placeholder="Jarvis"
-                // minLength={3}
-                value={lastName}
-                onChange={handleChangeLastName}
-              />
-              <br />
-              {isInputError && (
-                <p>Firstname and Name must be greater than 2 characters</p>
-              )}
-              <br />
-              <button type="submit" className="button is--save">
-                Save
-              </button>
-              <button className="button" onClick={cancel}>
-                Cancel
-              </button>
-            </form>
+            <Form
+              onChange={handleChange}
+              onSubmit={handleSubmit}
+              onCancel={handleCancel}
+              errorMessage={isInputError}
+              valueFirstName={formData.firstName}
+              valueLastName={formData.lastName}
+            />
           ) : null}
 
           {!isEdit ? (
