ecae1f89552460ae8fed4c40ff5c58e4af107fce
JBC <hello@jeanbaptistecalzia.fr>
2024-12-13 16:35:17 +0100

    Create editFormFieldSlice and update store

---
+++ app/src/redux/editFormFieldSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const editFormFieldSlice = createSlice({
+  name: "editFormField",
+  initialState: {
+    note: "",
+    category: "",
+  },
+  reducers: {
+    editInputCategory: (state, action) => {
+      state.input = action.payload;
+    },
+    editInputNote: (state, action) => {
+      state.note = action.payload;
+    },
+  },
+});
+
+export default editFormFieldSlice.reducer;
+export const { editInputCategory, editInputNote, category, note } =
+  editFormFieldSlice.actions;
